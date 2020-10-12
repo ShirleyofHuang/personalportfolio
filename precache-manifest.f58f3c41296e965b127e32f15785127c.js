e8c421e3d2c19e30bea5749f2be777ed6172fad9
ShirleyofHuang <shirley.huang2000@outlook.com>
2020-10-12 00:09:49 -0600

    Updates

---
+++ precache-manifest.f58f3c41296e965b127e32f15785127c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abf711ed09217b41fd5e75d73c67069",
+    "url": "/personalportfolio/index.html"
+  },
+  {
+    "revision": "2386c59e23f4c54747a0",
+    "url": "/personalportfolio/static/css/2.01fe66f5.chunk.css"
+  },
+  {
+    "revision": "d6eb1102e4719dac9428",
+    "url": "/personalportfolio/static/css/main.7c3f68d1.chunk.css"
+  },
+  {
+    "revision": "2386c59e23f4c54747a0",
+    "url": "/personalportfolio/static/js/2.ed9134fb.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/personalportfolio/static/js/2.ed9134fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6eb1102e4719dac9428",
+    "url": "/personalportfolio/static/js/main.cdb45b15.chunk.js"
+  },
+  {
+    "revision": "a6bede04cf10fd475013",
+    "url": "/personalportfolio/static/js/runtime-main.d67c2a1d.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/personalportfolio/static/media/logo.8dad8028.svg"
+  }
+]);
